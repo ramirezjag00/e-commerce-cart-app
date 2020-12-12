1fea2226671a92646468ce26bb6aeadddc7da611
Andrey Ramirez <ramirez.jag00@gmail.com>
2020-12-12 23:27:43 +0800

    Set up path aliasing

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,32 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@api': './src/api',
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@customtypes': './src/types',
+          '@routes': './src/routes',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
