5990c82456167ab36085a3998f1a09969b39a50e
Andrey Ramirez <ramirez.jag00@gmail.com>
2020-12-14 01:24:12 +0800

    Create ProductsScreen

--- src/components/screens/Products/ProductsScreen.tsx
+++ src/components/screens/Products/ProductsScreen.tsx
@@ -1,12 +1,45 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { ScrollView } from 'react-native'
+import EStyleSheet from 'react-native-extended-stylesheet'
+
+import Product from '@customtypes/product'
+import products from '../../../data/products.json'
+import Categories from './components/Categories'
+
+const DATA_CATEGORIES = products.items
+  .reduce((final: string[], obj: Product) => {
+    if (!final.includes(obj.category)) {
+      return [...final, obj.category]
+    } else {
+      return [...final]
+    }
+  }, [])
+  .sort()
 
 const ProductsScreen: React.FC = () => {
+  const [activeCategory, setCategory] = useState<string>('')
+
+  useEffect(() => {
+    if (DATA_CATEGORIES.length) {
+      setCategory(DATA_CATEGORIES[0])
+    }
+  }, [])
+
   return (
-    <View>
-      <Text>ProductsScreen!</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Categories
+        data={DATA_CATEGORIES}
+        setCategory={setCategory}
+        activeCategory={activeCategory}
+      />
+    </ScrollView>
   )
 }
+
+const styles = EStyleSheet.create({
+  container: {
+    backgroundColor: '$white',
+  },
+})
 
 export default ProductsScreen
