8567f95717ad73df15122855e423d7ff9d96ef9f
Andrey Ramirez <ramirez.jag00@gmail.com>
2020-12-13 22:52:12 +0800

    Change string numbers to numbers

--- src/api/csv_to_json.py
+++ src/api/csv_to_json.py
@@ -1,20 +1,20 @@
 import json
 import csv
 
-with open ("data/Products.csv", "r") as f:
+with open ("Products.csv", "r") as f:
     reader = csv.reader(f)
     next(reader)
     data = {"items": []}
     for row in reader:
         print (row)
         data["items"].append({
-            "id": row[0],
+            "id": int(row[0]),
             "name": row[1],
-            "barcode": row[2],
-            "price": row[3],
+            "barcode": int(row[2]),
+            "price": float(row[3]),
             "brand": row[4],
             "category": row[5]
         })
 
-with open ("data/products.json", "w") as f:
+with open ("products.json", "w") as f:
     json.dump(data, f, indent=2)
