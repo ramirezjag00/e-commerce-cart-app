a95d1838caf5de1feb25cb2a7849aa787adee495
Andrey Ramirez <ramirez.jag00@gmail.com>
2020-12-13 19:54:12 +0800

    Create theme constants

---
+++ src/constants/theme.ts
@@ -0,0 +1,33 @@
+const colors = {
+  $black: '#000000',
+  $white: '#ffffff',
+}
+
+const sizes = {
+  $s10: '0.625rem',
+  $s11: '0.6875rem',
+  $s12: '0.75rem',
+  $s13: '0.8125rem',
+  $s14: '0.875rem',
+  $s15: '0.9375rem',
+  $s16: '1rem',
+  $s17: '1.0625rem',
+  $s18: '1.125rem',
+  $s19: '1.1875rem',
+  $s20: '1.25rem',
+}
+
+const fontFamilies = {
+  $normal: 'GothamRounded-Book',
+  $bold: 'GothamRounded-Bold',
+  $medium: 'GothamRounded-Medium',
+  $italic: 'GothamRounded-BookItalic',
+}
+
+const theme = {
+  colors,
+  sizes,
+  fontFamilies,
+}
+
+export default theme
