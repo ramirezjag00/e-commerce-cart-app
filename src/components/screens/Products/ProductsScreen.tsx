71ad6020c6017de1f4eeb8bdc113c503793c1898
Andrey Ramirez <ramirez.jag00@gmail.com>
2020-12-15 01:41:03 +0800

    Implement CartButton

--- src/components/screens/Products/ProductsScreen.tsx
+++ src/components/screens/Products/ProductsScreen.tsx
@@ -6,10 +6,21 @@ import Categories from './components/Categories/Categories'
 import products from '../../../data/products.json'
 import Products from './components/Products/Products'
 import categories from '@utils/categories'
+import { useTypedSelector } from '@utils/store'
+import CartButton from './components/CartButton'
 
 const ProductsScreen: React.FC = () => {
   const [activeCategory, setCategory] = useState<string>(categories[0])
   const [activeIndex, setActiveIndex] = useState<number>(0)
+  const cart = useTypedSelector((store) => store.cart)
+  const cartTotal = cart.reduce(
+    (total, cartProduct) => total + cartProduct.amount,
+    0,
+  )
+  const cartQuantity = cart.reduce(
+    (total, cartProduct) => total + cartProduct.quantity,
+    0,
+  )
 
   useEffect(() => {
     setActiveIndex(categories.indexOf(activeCategory))
@@ -27,12 +38,16 @@ const ProductsScreen: React.FC = () => {
         products={products.items}
         activeIndex={activeIndex}
       />
+      {!!cart.length && (
+        <CartButton total={cartTotal} quantity={cartQuantity} />
+      )}
     </View>
   )
 }
 
 const styles = EStyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: '$white',
   },
 })
