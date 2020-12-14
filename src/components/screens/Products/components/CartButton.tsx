32f41fe22b2491740942013871fb18b85b6475ef
Andrey Ramirez <ramirez.jag00@gmail.com>
2020-12-15 01:40:30 +0800

    Add CartButton Component

---
+++ src/components/screens/Products/components/CartButton.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+import EStyleSheet from 'react-native-extended-stylesheet'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+
+interface Props {
+  total: number
+  quantity: number
+}
+
+const CartButton: React.FC<Props> = (props) => {
+  const { total, quantity } = props
+  const onPressCartButton = (): void => console.log('hello')
+
+  return (
+    <View style={styles.cartButtonContainer}>
+      <TouchableOpacity onPress={onPressCartButton} style={styles.cartButton}>
+        <View style={styles.icon}>
+          <Icon name={'cart'} size={25} color={EStyleSheet.value('$white')} />
+          <View style={styles.badge}>
+            <Text style={styles.badgeLabel}>{quantity}</Text>
+          </View>
+        </View>
+        <Text style={styles.cartButtonLabel}>CART - ₱{total.toFixed(2)}</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = EStyleSheet.create({
+  cartButtonContainer: {
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+    padding: '$s20',
+    backgroundColor: '$white',
+  },
+  cartButton: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: '$s12',
+    paddingHorizontal: '$s20',
+    backgroundColor: '$laurel',
+    borderRadius: 6,
+  },
+  cartButtonLabel: {
+    fontSize: '$s20',
+    color: '$white',
+    fontFamily: '$medium',
+    paddingLeft: '$s20',
+  },
+  icon: {
+    position: 'relative',
+  },
+  badge: {
+    backgroundColor: '$beautyBush',
+    borderRadius: '$s12',
+    position: 'absolute',
+    top: -5,
+    right: -5,
+    paddingVertical: 2,
+    paddingHorizontal: 4,
+  },
+  badgeLabel: {
+    fontSize: '$s12',
+    color: '$white',
+    fontFamily: '$bold',
+  },
+})
+
+export default CartButton
